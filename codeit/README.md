fd8e7db3858d55d5197af21437167fc5535a17fe
JaeGeunBang <jgb710@naver.com>
2022-07-06 22:00:06 +0900

    React 기초: 참조형 State 개념

--- codeit/README.md
+++ codeit/README.md
@@ -113,3 +113,48 @@ return (
     </div>
 );
 ```
+
+### 참조형 State
+기본형이 아닌 배열, 객체 같은 얘들은 참조형 State
+
+```javascript
+const [num, setNum] = useState(1);
+const [sum, setSum] = useState(0) ;
+const [gameHistory, setGameHistory] = useState([]);
+
+function random(n) {
+    return Math.ceil(Math.random() * n);
+}
+
+const handlerRollClick = () => {
+    const nextNum = random(6)
+    setNum(nextNum);
+    setSum(sum + nextNum)
+    setGameHistory([...gameHistory, nextNum]); // 참조형 객체는 아래처럼 만들어야 함
+}
+
+const handlerClearClick = () => {
+    setNum(1);
+    setSum(0);
+    setGameHistory([]);
+}
+
+return (
+    <div>
+        <div>
+            <Button onClick={handlerRollClick}>던지기</Button>
+            <Button onClick={handlerClearClick}>처음부터</Button>
+        </div>
+        <Dice color="red" num={num} />
+        <div>
+            <h2>나</h2>
+            <Dice color='blue' num={num} />
+            <h2>총점</h2>
+            <p>{sum}</p>
+            <h2>기록</h2>
+            {gameHistory.join(', ')}
+        </div>
+    </div>
+
+);
+```
--- codeit/basic/src/App.js
+++ codeit/basic/src/App.js
@@ -4,6 +4,8 @@ import {useState} from 'react';
 
 function App() {
     const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0) ;
+    const [gameHistory, setGameHistory] = useState([]);
 
     function random(n) {
         return Math.ceil(Math.random() * n);
@@ -12,10 +14,14 @@ function App() {
     const handlerRollClick = () => {
         const nextNum = random(6)
         setNum(nextNum);
+        setSum(sum + nextNum)
+        setGameHistory([...gameHistory, nextNum]);
     }
 
     const handlerClearClick = () => {
         setNum(1);
+        setSum(0);
+        setGameHistory([]);
     }
 
     return (
@@ -25,7 +31,16 @@ function App() {
                 <Button onClick={handlerClearClick}>처음부터</Button>
             </div>
             <Dice color="red" num={num} />
+            <div>
+                <h2>나</h2>
+                <Dice color='blue' num={num} />
+                <h2>총점</h2>
+                <p>{sum}</p>
+                <h2>기록</h2>
+                {gameHistory.join(', ')}
+            </div>
         </div>
+
     );
 }
 
