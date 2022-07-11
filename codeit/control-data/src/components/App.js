9d46e62910aefe0580a5b7937eb5b5f4403dd540
JaeGeunBang <jgb710@naver.com>
2022-07-11 20:20:33 +0900

    React 기초: 페이지네이션 구현

--- codeit/control-data/src/components/App.js
+++ codeit/control-data/src/components/App.js
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react';
 import ReviewList from './ReviewList';
 import { getReviews } from '../api';
 
+const LIMIT = 6;
+
 function App() {
     const [order, setOrder] = useState('createdAt');
+    const [offset, setOffset] = useState(0);
+    const [hasNext, setHasNext] = useState(false);
     const [items, setItems] = useState([]);
     const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,18 +20,27 @@ function App() {
         setItems(nextItems);
     };
 
-    const handleLoad = async () => {
-        const { reviews } = await getReviews();
-        setItems(reviews);
+    const handleLoad = async (options) => {
+        const { paging, reviews } = await getReviews(options);
+        if (options.offset === 0) {
+            setItems(reviews);
+        } else {
+            setItems([...items, ...reviews]);
+        }
+        setOffset(options.offset + options.limit);
+        setHasNext(paging.hasNext);
+    };
+
+    const handleLoadMore = async () => {
+        await handleLoad({ order, offset, limit: LIMIT });
     };
 
     // 홈페이지 리로드 할때마다 호출됨
     //// useEffect를 안쓰고 handleLoad()를 밖에다 쓰면, 무한 루프 발생 (데이터 가져오고, 렌더링되고 다시 데이터 가져오고..)
     //// useEffect와 빈배열을 쓰면 데이터 한번 가져오고 렌더링되고 끝
     useEffect(() => {
-        console.log('a')
-        handleLoad();
-    }, []);
+        handleLoad({ order, offset: 0, limit: LIMIT });
+    }, [order]);
 
     return (
         <div>
@@ -36,6 +49,10 @@ function App() {
                 <button onClick={handleBestClick}>베스트순</button>
             </div>
             <ReviewList items={sortedItems} onDelete={handleDelete} />
+            {hasNext && <button onClick={handleLoadMore}>
+                더 보기
+            </button>
+            }
         </div>
     );
 }
