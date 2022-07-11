e2561a0903a2181e037d446559ebf381c2f4c04a
JaeGeunBang <jgb710@naver.com>
2022-07-11 21:09:01 +0900

    React 기초: 파일 입력폼 생성

--- codeit/control-data/src/components/App.js
+++ codeit/control-data/src/components/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import ReviewList from './ReviewList';
+import ReviewForm from './ReviewForm';
 import { getReviews } from '../api';
-import ReviewForm from "./ReviewForm";
 
 const LIMIT = 6;
 
@@ -50,9 +50,6 @@ function App() {
         await handleLoad({ order, offset, limit: LIMIT });
     };
 
-    // 홈페이지 리로드 할때마다 호출됨
-    //// useEffect를 안쓰고 handleLoad()를 밖에다 쓰면, 무한 루프 발생 (데이터 가져오고, 렌더링되고 다시 데이터 가져오고..)
-    //// useEffect와 빈배열을 쓰면 데이터 한번 가져오고 렌더링되고 끝
     useEffect(() => {
         handleLoad({ order, offset: 0, limit: LIMIT });
     }, [order]);
@@ -63,7 +60,7 @@ function App() {
                 <button onClick={handleNewestClick}>최신순</button>
                 <button onClick={handleBestClick}>베스트순</button>
             </div>
-            <ReviewForm></ReviewForm>
+            <ReviewForm />
             <ReviewList items={sortedItems} onDelete={handleDelete} />
             {hasNext && (
                 <button disabled={isLoading} onClick={handleLoadMore}>
